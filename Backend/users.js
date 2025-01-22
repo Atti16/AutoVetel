2a65c8b02072c8f751a1ee60a4b90453dbf4646a
Atti16 <84785429+Atti16@users.noreply.github.com>
2025-01-22 10:57:40 +0100

    Add files via upload

---
+++ Backend/users.js
@@ -0,0 +1,7 @@
+const userDB = [
+    { id: 101, email: "atti@atti", pass: "pass" },
+    { id: 102, email: "", pass: "pass" },
+    { id: 103, email: "test@test", pass: "test" }
+];
+
+module.exports = { userDB }
